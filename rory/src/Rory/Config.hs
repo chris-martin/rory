41f01ccf0d6059a22a367343fb2f0b6d3f6ecb7b
Chris Martin <ch.martin@gmail.com>
2016-03-12 04:36:37 -0800

    reload config on HUP signal

--- rory/src/Rory/Config.hs
+++ rory/src/Rory/Config.hs
@@ -1,1 +1,22 @@
-module Rory.Config () where
+module Rory.Config (load, Config(..)) where
+
+import           Rory.Args (Args)
+import qualified Rory.Args as Args
+
+import qualified Data.ByteString as BS
+
+data Config = Config
+    { fileContent :: Maybe BS.ByteString
+    } deriving Show
+
+empty :: Config
+empty = Config { fileContent = Nothing }
+
+fromFileContent :: BS.ByteString -> Config
+fromFileContent x = Config { fileContent = Just x }
+
+load :: Args -> IO Config
+load args = maybe (return empty) loadFile $ Args.configFile args
+
+loadFile :: FilePath -> IO Config
+loadFile path = fromFileContent <$> BS.readFile path
--- rory/src/Rory/Server.hs
+++ rory/src/Rory/Server.hs
@@ -2,8 +2,10 @@
 
 module Rory.Server (run) where
 
-import           Rory.Args (Args)
-import qualified Rory.Args
+import           Rory.Args        (Args)
+import qualified Rory.Args        as Args
+import           Rory.Config      (Config)
+import qualified Rory.Config      as Config
 import qualified Rory.Server.Name
 
 import qualified Blaze.ByteString.Builder.ByteString as BBS
@@ -11,26 +13,49 @@ import           Control.Monad.Trans.Resource
 import qualified Control.Monad.Trans.Resource        as Res
 import qualified Data.ByteString                     as BS
 import qualified Data.ByteString.Char8               as S8
+import           Data.IORef                          (IORef, newIORef,
+                                                      readIORef, writeIORef)
 import           Network.HTTP.Types                  (status200)
 import qualified Network.Wai                         as Wai
-import       qualified    Network.Wai.Handler.Warp            as Warp
+import qualified Network.Wai.Handler.Warp            as Warp
 import qualified Network.Wai.Parse                   as NWP
 import           System.Directory                    (renameFile)
 import qualified System.Posix.Signals                as Sig
 
 run :: Args -> IO ()
 run args = do
-    _    <- Sig.installHandler Sig.sigHUP Sig.Ignore Nothing
-    _    <- Warp.run 3000 $ middleware $ application $ initServer args
+    server <- initServer args
+    _ <- installHupHandler server
+    Warp.run 3000 $ middleware $ application server
+  where
+    middleware = Rory.Server.Name.middleware
+
+-- | Set up the HUP signal handler to reload the config.
+installHupHandler :: Server -> IO ()
+installHupHandler server = do
+    _ <- Sig.installHandler Sig.sigHUP handler Nothing
     return ()
-  where middleware = Rory.Server.Name.middleware
+  where
+    handler = Sig.Catch $ do
+        putStrLn "Received HUP signal; reloading config"
+        loadConfig server
 
 data Server = Server
-    { serverArgs :: Args
+    { serverArgs   :: Args
+    , serverConfig :: IORef Config
     }
 
-initServer :: Args -> Server
-initServer args = Server { serverArgs = args }
+initServer :: Args -> IO Server
+initServer args = do
+    config <- Config.load args
+    configVar <- newIORef config
+    return Server { serverArgs   = args
+                  , serverConfig = configVar }
+
+loadConfig :: Server -> IO ()
+loadConfig server = do
+   config <- Config.load $ serverArgs server
+   writeIORef (serverConfig server) config
 
 application :: Server -> Wai.Application
 application server request respond = do
@@ -38,7 +63,8 @@ application server request respond = do
         (_params, files) <- NWP.parseRequestBody (tempFileBackEnd is) request
         _ <- renameFile (NWP.fileContent $ snd $ head files) "/tmp/upload"
         return ()
-    received <- respond $ stringResponse $ show $ serverArgs server
+    config <- readIORef $ serverConfig server
+    received <- respond $ stringResponse $ show config
     return received
 
 tempFileBackEnd :: Res.InternalState -> ignored1 -> ignored2 -> IO BS.ByteString -> IO FilePath
