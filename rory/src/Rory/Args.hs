2ddd474ac04ca8f5d8ea43d191bb1f9224b2dd06
Chris Martin <ch.martin@gmail.com>
2016-03-12 18:28:32 -0800

    Add command arg
    Related to #14

--- rory/src/Rory/Args.hs
+++ rory/src/Rory/Args.hs
@@ -16,14 +16,27 @@ parserInfo = info (helper <*> parser) fullDesc
 data Args = Args
     { configFile :: Maybe FilePath
     , pidFile    :: Maybe FilePath
+    , command    :: Maybe Command
     } deriving Show
+
+data Command = Start | Stop | Restart | Reload deriving Show
+
+commandOption :: ReadM Command
+commandOption = str >>= \s -> case s of
+    "start"   -> pure Start
+    "stop"    -> pure Stop
+    "restart" -> pure Restart
+    "reload"  -> pure Reload
+    _         -> readerError $ "Invalid command: '" ++ s ++ "'"
 
 parser :: Parser Args
 parser = Args
     <$> (optional $ strOption
           ( long "config-file"
          <> short 'c'
-         <> metavar "CONFIG_FILE" ))
+         <> metavar "CONFIG_FILE"))
     <*> (optional $ strOption
           ( long "pid-file"
-         <> metavar "PID_FILE" ))
+         <> metavar "PID_FILE"))
+    <*> (optional $ argument commandOption
+          (metavar "COMMAND"))
