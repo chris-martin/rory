5d1736d1d619affe7f7e3467c4203a78a082d607
Chris Martin <ch.martin@gmail.com>
2016-03-13 19:30:15 -0700

    circle nix stuff

--- circle-setup.sh
+++ circle-setup.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-curl https://nixos.org/nix/install | sh
-
 export PATH=$HOME/.nix-profile/bin:$PATH
+export CURL_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
+export NIX_PATH=$HOME/.nix-defexpr/channels
+
+curl https://nixos.org/nix/install | sh
 
 nix-env -i haskellPackages.stack
 
-stack --no-terminal setup
-stack --no-terminal install hscolour
+stack --nix --no-terminal setup
+stack --nix --no-terminal install hscolour
