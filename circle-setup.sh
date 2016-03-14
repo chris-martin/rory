deec24941a8ae78e6cd7cb7ae3151e43918e057c
Chris Martin <ch.martin@gmail.com>
2016-03-13 20:04:15 -0700

    Revert "circle: ~/nix-store"
    This reverts commit 207fd37382ec31daeebe00e14803ebaa55091610.

--- circle-setup.sh
+++ circle-setup.sh
@@ -3,9 +3,6 @@
 export PATH=$HOME/.nix-profile/bin:$PATH
 export CURL_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
 export NIX_PATH=$HOME/.nix-defexpr/channels
-export NIX_STORE_DIR=$HOME/nix-store
-
-mkdir -p ~/nix-store
 
 curl https://nixos.org/nix/install | sh
 
