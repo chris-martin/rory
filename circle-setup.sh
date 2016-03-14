43f70687c64b6b9fad4ba603cd293360aef0d510
Chris Martin <ch.martin@gmail.com>
2016-03-13 18:53:19 -0700

    circle with nix

--- circle-setup.sh
+++ circle-setup.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 
-pushd $(mktemp -d)
-wget https://github.com/commercialhaskell/stack/releases/download/v1.0.4/stack-1.0.4.2-linux-x86_64.tar.gz
-tar -xzf stack-1.0.4.2-linux-x86_64.tar.gz
-sudo mv stack-1.0.4.2-linux-x86_64/stack /usr/bin/
-popd
+curl https://nixos.org/nix/install | sh
 
-sudo apt-get install -y libgmp-dev libsystemd-dev
+nix-env -i haskellPackages.stack
 
 stack --no-terminal setup
 stack --no-terminal install hscolour
