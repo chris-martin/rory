176bb18443f0d95d9ee24323e5e4a6e37febb063
Chris Martin <ch.martin@gmail.com>
2016-03-19 02:29:17 -0700

    require GET or PUT
    closes #5

--- rory/src/Rory/Server.hs
+++ rory/src/Rory/Server.hs
@@ -7,22 +7,27 @@ import qualified Rory.Config  as Config
 import qualified Rory.Version
 
 import qualified Blaze.ByteString.Builder.ByteString as BBS
+
 import           Control.Monad.Trans.Resource
-import qualified Control.Monad.Trans.Resource        as Res
-import qualified Data.ByteString                     as BS
-import qualified Data.ByteString.Char8               as S8
-import           Data.IORef                          (IORef, newIORef,
-                                                      readIORef, writeIORef)
-import           Data.List                           (intercalate)
-import qualified Data.Text                           as Text
-import           Network.HTTP.Types                  (hContentType, status200)
-import qualified Network.HTTP.Types                  as H
-import qualified Network.Wai                         as Wai
-import qualified Network.Wai.Handler.Warp            as Warp
-import qualified Network.Wai.Parse                   as NWP
-import           System.Directory                    (renameFile)
-import qualified System.Posix.Signals                as Sig
-import qualified Systemd.Journal                     as J
+import qualified Control.Monad.Trans.Resource as Res
+
+import           Data.ByteString       (ByteString)
+import qualified Data.ByteString       as BS
+import qualified Data.ByteString.Char8 as S8
+import           Data.IORef            (IORef, newIORef, readIORef, writeIORef)
+import           Data.List             (intercalate)
+import qualified Data.Text             as Text
+
+import           Network.HTTP.Types       (hContentType, status200, status405)
+import qualified Network.HTTP.Types       as H
+import qualified Network.Wai              as Wai
+import qualified Network.Wai.Handler.Warp as Warp
+import qualified Network.Wai.Parse        as NWP
+
+import           System.Directory     (renameFile)
+import qualified System.Posix.Signals as Sig
+
+import qualified Systemd.Journal as J
 
 run :: Args -> IO ()
 run args = do
@@ -80,19 +85,26 @@ loadConfig server = do
 
 application :: Server -> Wai.Application
 application server request respond = do
+    case H.parseMethod $ Wai.requestMethod request of
+        Right(H.GET) -> respond $ plain status200 $ S8.pack "Hi.\n"
+        Right(H.PUT) -> put server request respond
+        _ -> respond $ plain status405 $ S8.pack "Only GET and PUT are supported."
+
+put :: Server -> Wai.Application
+put server request respond = do
     _ <- runResourceT $ Res.withInternalState $ \is -> do
         (_params, files) <- NWP.parseRequestBody (tempFileBackEnd is) request
         _ <- renameFile (NWP.fileContent $ snd $ head files) "/tmp/upload"
         return ()
     config <- readIORef $ serverConfig server
-    received <- respond $ stringResponse $ show config
+    received <- respond $ plain status200 $ S8.pack $ (show config ++ "\n")
     return received
 
 tempFileBackEnd :: Res.InternalState -> ignored1 -> ignored2 -> IO BS.ByteString -> IO FilePath
 tempFileBackEnd = NWP.tempFileBackEndOpts (pure "/tmp") "webenc.buf"
 
-stringResponse :: String -> Wai.Response
-stringResponse body = Wai.responseBuilder
-    status200
+plain :: H.Status -> ByteString -> Wai.Response
+plain status body = Wai.responseBuilder
+    status
     [(hContentType, S8.pack "text/plain")]
-    (BBS.fromByteString $ S8.pack body)
+    (BBS.fromByteString $ body)
